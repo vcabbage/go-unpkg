ae10d355cfaed925193f94e6c48daebf0eb97828
Kale Blankenship <kale@lemnisys.com>
2016-10-21 18:01:26 -0700

    Remove CLI

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/vcabbage/go-unpkg/cli"
 	"github.com/vcabbage/go-unpkg/server"
 )
 
 func main() {
-	os.Exit(run())
-}
-
-func run() int {
-	if len(os.Args) < 2 {
-		fmt.Println(`must specify "serve" or package`)
-		return 1
-	}
-
-	if os.Args[1] == "serve" {
-		return server.Run()
-	}
-
-	return cli.Run()
+	os.Exit(server.Run())
 }
